903f581c2634085fb849c3e7167b66416d42907e
Markivanarcega01 <arcegamarkivan@gmail.com>
2025-08-01 16:30:51 +0800

    Change error message from user verification

--- server/middleware/verifyUser.js
+++ server/middleware/verifyUser.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const verify = (req,res,next)=>{
-    const token = req.headers.authorization?.split(' ')[1];
-    if(!token){
-        return res.status(401).json({
-            error:"Missing Token"
-        })
-    }   
+const verify = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(400).json({
+      error: "Missing Token",
+    });
+  }
 
-    try {
-        const user = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = user;
-        next();
-    } catch (error) {
-        return res.status(403).json({error: error})
-    }
-}
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(403).json({ error: "Invalid token" });
+  }
+};
 
-module.exports = {verify}
+module.exports = { verify };
