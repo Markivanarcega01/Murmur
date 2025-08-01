701907676539a9da3ceaba9eb3085bbfe99a3aed
Markivanarcega01 <arcegamarkivan@gmail.com>
2025-08-01 16:28:56 +0800

    Changed fetch to Axios

--- client/src/api/messages.api.ts
+++ client/src/api/messages.api.ts
@@ -1,17 +1,19 @@
+import axios from "axios";
+
 export const messagesApi = () => {
   const getMessagesApi = async (conversationId: number) => {
     const token = sessionStorage.getItem("token");
-    const response = await fetch(
+
+    const response = await axios.get(
       `http://127.0.0.1:3000/api/v1/show-messages/${conversationId}`,
       {
-        method: "GET",
         headers: {
           Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
         },
       }
     );
-    return response.json();
+    console.log(response.data);
+    return response.data;
   };
 
   return { getMessagesApi };
--- client/src/api/users.api.ts
+++ client/src/api/users.api.ts
@@ -1,32 +1,36 @@
 import { UserLoginDataProps } from "../interface/users.interface";
+import axios from "axios";
 
 export const usersApi = () => {
   const getUsersApi = async () => {
     const token = sessionStorage.getItem("token");
-    const response = await fetch("http://127.0.0.1:3000/api/v1/get-users", {
-      method: "GET",
+
+    const response = await axios.get("http://127.0.0.1:3000/api/v1/get-users", {
       headers: {
         Authorization: `Bearer ${token}`,
-        "Content-Type": "applicaiton/json",
       },
     });
 
-    return response.json();
+    return response.data;
   };
 
   const loginUsersApi = async (credentials: UserLoginDataProps) => {
-    const response = await fetch("http://127.0.0.1:3000/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: credentials.username,
-        password: credentials.password,
-      }),
+    // const response = await fetch("http://127.0.0.1:3000/auth/login", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     username: credentials.username,
+    //     password: credentials.password,
+    //   }),
+    // });
+    const response = await axios.post("http://127.0.0.1:3000/auth/login", {
+      username: credentials.username,
+      password: credentials.password,
     });
 
-    return response.json();
+    return response.data;
   };
 
   return { getUsersApi, loginUsersApi };
