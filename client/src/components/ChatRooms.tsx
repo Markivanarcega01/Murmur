d2076cbd8f0be2e4ab2fb94a3738eb781abcbb3b
Markivanarcega01 <arcegamarkivan@gmail.com>
2025-08-07 15:48:32 +0800

    Update: Made the first Chatrooms Component

---
+++ client/src/components/ChatRooms.tsx
@@ -0,0 +1,100 @@
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
+import { useState } from "react";
+import UserChatInformation from "./UserChatInformation";
+import { ParticipantConversationsDataProps } from "../interface/conversation-participants.interface";
+import { UsersDataProps } from "../interface/users.interface";
+import { getConversationDisplayName } from "../shared/utils/getConversationDisplayName";
+
+export default function ChatRooms({
+  participantConversations,
+  loggedUser,
+  setSelected,
+}: {
+  participantConversations: ParticipantConversationsDataProps[];
+  loggedUser: UsersDataProps;
+  setSelected: React.Dispatch<React.SetStateAction<number>>;
+}) {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const handleScroll = (e: React.UIEvent<HTMLElement>) => {
+    const { scrollTop } = e.target as HTMLElement;
+    setIsScrolled(scrollTop > 0);
+  };
+  return (
+    <div className="basis-1/4 p-4 flex flex-col h-full ">
+      <div className="flex flex-row h-fit space-x-3">
+        <div className="text-2xl font-semibold basis-10/12 self-center">
+          Chats
+        </div>
+        <button className="rounded-full p-1 bg-gray-200 w-9">
+          <MoreHorizIcon />
+        </button>
+        <button className="rounded-full p-1 bg-gray-200 w-9">
+          <CreateOutlinedIcon />
+        </button>
+      </div>
+
+      <form className="max-w-md mt-4 mb-4">
+        <div className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full h-10 p-4 ps-10 text-sm rounded-full bg-gray-200"
+            placeholder="Search Messenger"
+            required
+          />
+        </div>
+      </form>
+
+      {/*Add is Selected for each users */}
+      <div
+        className={`flex-1 overflow-auto ${
+          isScrolled ? "border-y border-slate-200" : ""
+        }`}
+        onScroll={handleScroll}
+      >
+        {participantConversations?.map(
+          (data: ParticipantConversationsDataProps) => {
+            let name = getConversationDisplayName(data, loggedUser.username);
+            const lastConvo =
+              data.Messages.length !== 0 ? data.Messages[0].text : "";
+            return (
+              <div
+                onClick={(e) => {
+                  setSelected(data.id);
+                }}
+                key={data.id}
+              >
+                <UserChatInformation
+                  profile={data.participants[0].profile_picture}
+                  information={{
+                    name: name,
+                    lastConvo: lastConvo,
+                  }}
+                  isActive={true}
+                />
+              </div>
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
+}
--- client/src/page/Message.tsx
+++ client/src/page/Message.tsx
@@ -1,22 +1,11 @@
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
-import UserChatInformation from "../components/UserChatInformation";
-import CallIcon from "@mui/icons-material/Call";
-import VideocamIcon from "@mui/icons-material/Videocam";
-import InfoIcon from "@mui/icons-material/Info";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import ImageIcon from "@mui/icons-material/Image";
-import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
-import GifIcon from "@mui/icons-material/Gif";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
-import ChatBlock from "../components/ChatBlock";
 import ChatBlockInformation from "../components/ChatBlockInformation";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { userService } from "../services/user.service";
 import { messageService } from "../services/message.service";
 import { Navigate } from "react-router";
 import { conversationParticipantsService } from "../services/conversation-participants.service";
-import { ParticipantConversationsDataProps } from "../interface/conversation-participants.interface";
+import ChatRooms from "../components/ChatRooms";
+import ChatRoomMessages from "../components/ChatRoomMessages";
 
 const { getUser } = userService();
 const { getMessages } = messageService();
@@ -24,307 +13,81 @@ const { getParticipantConversations } = conversationParticipantsService();
 
 function Message() {
   const [selected, setSelected] = useState<number>(1);
-  const [conversationData, setConversationData] =
-    useState<ParticipantConversationsDataProps>();
-  console.log(conversationData);
+
   {
     /** Fetch User */
   }
-  const { data: loggedUser, isLoading, isError } = getUser();
-  console.log("user", loggedUser);
+  const { data: loggedUser, isLoading } = getUser();
   {
     /** Fetch Messages */
   }
-  const {
-    data: messages,
-    isLoading: messageLoading,
-    error,
-  } = getMessages(selected);
-
-  // console.log(isError);
-  //console.log(error);
-  //console.log(messages);
+  const { data: messages, isLoading: messageLoading } = getMessages(selected);
 
   {
     /** Fetch Participant Conversations */
   }
-
   const {
     data: participantConversations,
     isLoading: participantConversationsIsLoading,
     isError: participantConversationsIsError,
   } = getParticipantConversations();
 
-  const [isScrolled, setIsScrolled] = useState(false);
+  const participantConversationsSelectedValue = useMemo(() => {
+    return participantConversations?.find(
+      (conversation) => conversation.id === selected
+    );
+  }, [participantConversations, selected]);
 
-  const handleScroll = (e: React.UIEvent<HTMLElement>) => {
-    const { scrollTop } = e.target as HTMLElement;
-    setIsScrolled(scrollTop > 0);
-  };
-  const dummyData = [
-    {
-      id: 1,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Ivan Arcega",
-      lastConvo: "This is a sample convo",
-      isActive: true,
-    },
-    {
-      id: 2,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Stephen Arcega",
-      lastConvo: "This is a sample1 convo",
-    },
-    {
-      id: 3,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Ivan Arcega",
-      lastConvo: "This is a sample convo",
-    },
-    {
-      id: 4,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Stephen Arcega",
-      lastConvo: "This is a sample1 convo",
-    },
-    {
-      id: 5,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Ivan Arcega",
-      lastConvo: "This is a sample convo",
-    },
-    {
-      id: 6,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Stephen Arcega",
-      lastConvo: "This is a sample1 convo",
-    },
-    {
-      id: 7,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Ivan Arcega",
-      lastConvo: "This is a sample convo",
-    },
-    {
-      id: 8,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Stephen Arcega",
-      lastConvo: "This is a sample1 convo",
-    },
-    {
-      id: 9,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Ivan Arcega",
-      lastConvo: "This is a sample convo",
-    },
-    {
-      id: 10,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Stephen Arcega",
-      lastConvo: "This is a sample1 convo",
-    },
-    {
-      id: 11,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Ivan Arcega",
-      lastConvo: "This is a sample convo",
-    },
-    {
-      id: 12,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Stephen Arcega",
-      lastConvo: "This is a sample1 convo",
-    },
-    {
-      id: 13,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Ivan Arcega",
-      lastConvo: "This is a sample convo",
-    },
-    {
-      id: 14,
-      profile: "/src/assets/UPOULogo.png",
-      name: "Mark Stephen Arcega",
-      lastConvo: "This is a sample1 convo",
-    },
-  ];
+  useEffect(() => {
+    if (participantConversations && participantConversations.length) {
+      setSelected(participantConversations[0].id);
+    }
+  }, [participantConversations]);
+
   if (participantConversationsIsError) {
     return <Navigate to={"/login"} />;
   }
+
   return (
     <>
       <div className="flex flex-row h-screen">
-        {/* Component 1*/}
-        <div className="basis-1/4 p-4 flex flex-col h-full ">
-          <div className="flex flex-row h-fit space-x-3">
-            <div className="text-2xl font-semibold basis-10/12 self-center">
-              Chats
-            </div>
-            <button className="rounded-full p-1 bg-gray-200 w-9">
-              <MoreHorizIcon />
-            </button>
-            <button className="rounded-full p-1 bg-gray-200 w-9">
-              <CreateOutlinedIcon />
-            </button>
-          </div>
-
-          <form className="max-w-md mt-4 mb-4">
-            <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
-              <input
-                type="search"
-                id="default-search"
-                className="block w-full h-10 p-4 ps-10 text-sm rounded-full bg-gray-200"
-                placeholder="Search Messenger"
-                required
-              />
-            </div>
-          </form>
-
-          {/*Add is Selected for each users */}
-          <div
-            className={`flex-1 overflow-auto ${
-              isScrolled ? "border-y border-slate-200" : ""
-            }`}
-            onScroll={handleScroll}
-          >
-            {participantConversations && !participantConversationsIsLoading
-              ? participantConversations?.map((data) => {
-                  let name =
-                    data.name != null
-                      ? data.name
-                      : data.participants.length <= 2
-                      ? data.participants
-                          .map((user) =>
-                            user.username !== loggedUser?.username
-                              ? `${user.firstname} ${user.lastname}`
-                              : ""
-                          )
-                          .join("") // join to convert array to string
-                      : data.participants
-                          .map((user) => user.firstname)
-                          .join(", ");
-                  const lastConvo =
-                    data.Messages.length !== 0 ? data.Messages[0].text : "";
-                  return (
-                    <div
-                      onClick={(e) => {
-                        setSelected(data.id);
-                        setConversationData(data);
-                      }}
-                      key={data.id}
-                    >
-                      <UserChatInformation
-                        profile={data.participants[0].profile_picture}
-                        information={{
-                          name: name,
-                          lastConvo: lastConvo,
-                        }}
-                        isActive={true}
-                      />
-                    </div>
-                  );
-                })
-              : "Loading"}
-          </div>
-        </div>
+        {participantConversations && loggedUser ? (
+          //Component 1
+          <ChatRooms
+            participantConversations={participantConversations}
+            loggedUser={loggedUser}
+            setSelected={setSelected}
+          />
+        ) : (
+          //Create a skeleton component
+          "Loading"
+        )}
 
         {/* Component 2 */}
-        <div className="basis-3/4 border-x flex flex-col">
-          <div className="border-b flex justify-between items-center">
-            <div className="w-fit">
-              {conversationData ? (
-                <UserChatInformation
-                  profile={conversationData.participants[0].profile_picture}
-                  information={{
-                    name:
-                      conversationData.name != null
-                        ? `${conversationData.name}`
-                        : conversationData.participants.length <= 2
-                        ? conversationData.participants
-                            .filter(
-                              (user) => user.username !== loggedUser?.username
-                            )
-                            .map((user) => `${user.firstname} ${user.lastname}`)
-                            .join("") // Only one other user in 1-on-1 chat
-                        : conversationData.participants
-                            .map((user) => user.firstname)
-                            .join(", "), // Group chat
-                    lastConvo: ``,
-                  }}
-                  isActive={false} // comes from the socket io, if the user is connected
-                />
-              ) : (
-                "Loading"
-              )}
-            </div>
-            <div className="flex flex-row gap-x-5 mr-5">
-              <button>
-                <CallIcon color="primary" />
-              </button>
-              <button>
-                <VideocamIcon color="primary" />
-              </button>
-              <button>
-                <InfoIcon color="primary" />
-              </button>
-            </div>
-          </div>
-          <div className="flex-1 overflow-auto px-2">
-            {messages && !messageLoading ? (
-              <ChatBlock conversationMessages={messages} />
-            ) : null}
-          </div>
-          <div className="p-2 flex flex-row gap-x-3 items-center">
-            <button>
-              <AddCircleIcon color="primary" />
-            </button>
-            <button>
-              <ImageIcon color="primary" />
-            </button>
-            <button>
-              <EmojiEmotionsIcon color="primary" />
-            </button>
-            <button>
-              <GifIcon color="primary" />
-            </button>
-            <div className="relative flex-1">
-              <button className="absolute inset-y-0 end-0 flex items-center pe-3 ">
-                <EmojiEmotionsIcon color="primary" />
-              </button>
-              <form action="">
-                <input
-                  className="block w-full h-8 px-4 pe-10 text-sm rounded-full bg-gray-200"
-                  placeholder="Search Messenger"
-                  required
-                />
-              </form>
-            </div>
-            <button>
-              <ThumbUpIcon color="primary" />
-            </button>
-          </div>
-        </div>
+        {participantConversationsSelectedValue && loggedUser && messages ? (
+          <ChatRoomMessages
+            participantConversationsSelectedValue={
+              participantConversationsSelectedValue
+            }
+            loggedUser={loggedUser}
+            messages={messages}
+          />
+        ) : (
+          //Create a skeleton component
+          "Loading Middle"
+        )}
 
         {/** Component 3*/}
         <div className="basis-1/4">
-          <ChatBlockInformation />
+          {participantConversationsSelectedValue && loggedUser ? (
+            <ChatBlockInformation
+              chatRoomDetails={participantConversationsSelectedValue}
+              loggedUser={loggedUser?.username}
+            />
+          ) : (
+            //Create a skeleton component
+            <p>Loading Chatroom Details</p>
+          )}
         </div>
       </div>
     </>
