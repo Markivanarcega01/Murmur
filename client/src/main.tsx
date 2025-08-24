d9d6dcfb7c02a507c9a198d3c3f057643adf6444
Markivanarcega01 <arcegamarkivan@gmail.com>
2025-08-24 19:12:00 +0800

    Apply: React Context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,23 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react";
 import { BrowserRouter } from "react-router";
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { CssBaseline } from '@mui/material';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MyStateProvider } from "./context/selectedContext.tsx";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   //<StrictMode>
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <CssBaseline />
-      <App />
+      <MyStateProvider>
+        <App />
+      </MyStateProvider>
     </BrowserRouter>
   </QueryClientProvider>
   //</StrictMode>,
-)
+);
